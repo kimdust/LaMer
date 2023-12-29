559aa43a1c2375bba9aae94597ef3135490f3fb4
kimdust <mji1223@naver.com>
2023-12-29 16:02:47 +0900

    jquery category

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,1 +1,8 @@
+$(function () {
+  $(".category swiper-slide:first-child").addClass("active");
 
+  $(".category swiper-slide a").on("click", function () {
+    $(".category swiper-slide").removeClass("active");
+    $(this).parent().addClass("active");
+  });
+});
