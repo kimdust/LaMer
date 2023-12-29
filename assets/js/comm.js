7b2dce051779c203d19273268a939ea52b3e82c6
kimdust <mji1223@naver.com>
2023-12-29 18:02:12 +0900

    sub1 finish

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -5,4 +5,13 @@ $(function () {
     $(".category swiper-slide").removeClass("active");
     $(this).parent().addClass("active");
   });
+
+  if ($(window).width() > 1280) {
+    $(".category_1280 li:first-child").addClass("active");
+
+    $(".category_1280 .category_list a").on("click", function () {
+      $(".category_1280 .category_list li").removeClass("active");
+      $(this).parent().addClass("active");
+    });
+  }
 });
