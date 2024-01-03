00157e85e945a48e45e9f2a6fcdfd82e454fbf38
kimdust <mji1223@naver.com>
2024-01-03 18:24:02 +0900

    0103 sub2

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -14,4 +14,24 @@ $(function () {
       $(this).parent().addClass("active");
     });
   }
+
+  $(".item_option label:first-child").addClass("active");
+
+  $(".item_option .option_col label").on("click", function () {
+    $(".item_option label").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $('input[type="radio"]').change(function () {
+    var selectedPrice = $(this).val();
+
+    $(".item_price").text("₩" + selectedPrice);
+  });
+
+  $(".details_nav li:first-child").addClass("active");
+
+  $(".details_nav .details_col li").on("click", function () {
+    $(".details_nav .details_col li").removeClass("active");
+    $(this).addClass("active");
+  });
 });
